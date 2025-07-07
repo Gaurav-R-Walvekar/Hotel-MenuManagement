9c2cedc0811e12c1cc0b5094dd25b3230925fbcf
Gaurav <gauravwalvekarr@gmail.com>
2025-07-07 15:52:07 +0530

    updated server url

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://hotel-menumanagement.onrender.com';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://hotelserver.aisoft.it.com:5000'; //'https://hotel-menumanagement.onrender.com';
 
 export const config = {
   API_BASE_URL,
